c252aec77f01277b8e1d7006ce5762d260bf1703
Kooreis <kooreis.official@gmail.com>
2024-12-25 20:57:31 -0600

    This is the main program that uses a Trie data structure to implement an autocomplete feature. It inserts a list of words into the Trie and then enters a loop where it prompts the user to enter a prefix and displays the words that start with that prefix.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple implementation of an autocomplete feature using a prefix search in C#:
-
-```C#
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,47 +22,3 @@ public class Program
         }
     }
 }
-
-public class TrieNode
-{
-    public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
-    public List<string> Words = new List<string>();
-}
-
-public class Trie
-{
-    private readonly TrieNode root = new TrieNode();
-
-    public void Insert(string word)
-    {
-        var node = root;
-        foreach (var ch in word)
-        {
-            if (!node.Children.ContainsKey(ch))
-            {
-                node.Children[ch] = new TrieNode();
-            }
-            node = node.Children[ch];
-        }
-        node.Words.Add(word);
-    }
-
-    public List<string> Search(string prefix)
-    {
-        var node = root;
-        foreach (var ch in prefix)
-        {
-            if (!node.Children.ContainsKey(ch))
-            {
-                return new List<string>();
-            }
-            node = node.Children[ch];
-        }
-        return node.Words;
-    }
-}
-```
-
-This program uses a data structure called a Trie to store the words. When a word is inserted, it is broken down into its individual characters and each character is used as a key in a dictionary. The value associated with each key is another TrieNode, which contains its own dictionary of children. This forms a tree-like structure where each path from the root to a leaf node represents a word.
-
-When a prefix is searched, the program traverses the Trie using the characters in the prefix. If it reaches a node that does not have a child for the next character in the prefix, it returns an empty list because there are no words with that prefix. If it successfully traverses all the characters in the prefix, it returns the list of words at the final node.
