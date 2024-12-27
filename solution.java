ba8a0b8645a06587f7a913f2504221590979f49a
Kooreis <kooreis.official@gmail.com>
2024-12-26 21:57:31 -0600

    This update introduces the AutocompleteSystem class. It has a root TrieNode and a prefix string. The constructor takes an array of sentences and inserts each one into the Trie. The insert method adds a word to the Trie.

--- solution.java
+++ solution.java
@@ -1,10 +1,23 @@
-import java.util.*;
+class AutocompleteSystem {
+    private TrieNode root;
+    private String prefix;
 
-class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean endOfWord;
-    public TrieNode() {
-        children = new HashMap<>();
-        endOfWord = false;
+    public AutocompleteSystem(String[] sentences) {
+        root = new TrieNode();
+        prefix = "";
+        for (String s : sentences) {
+            insert(s);
+        }
+    }
+
+    private void insert(String s) {
+        TrieNode curr = root;
+        for (char c : s.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.endOfWord = true;
     }
 }
