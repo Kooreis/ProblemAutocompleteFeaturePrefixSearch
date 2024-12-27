dbea892d79898ce304fb5b9b178fc2da0d7f962a
Kooreis <kooreis.official@gmail.com>
2024-12-26 21:57:31 -0600

    This is the TrieNode class. Each TrieNode has a dictionary of children, where each key is a character and each value is another TrieNode. It also has a list of words that end at this node.

--- solution.cs
+++ solution.cs
@@ -1,24 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class Program
+public class TrieNode
 {
-    static void Main(string[] args)
-    {
-        var words = new List<string> { "apple", "banana", "apricot", "blueberry", "cherry", "date", "fig", "grape" };
-        var trie = new Trie();
-        foreach (var word in words)
-        {
-            trie.Insert(word);
-        }
-
-        while (true)
-        {
-            Console.Write("Enter prefix: ");
-            var prefix = Console.ReadLine();
-            var results = trie.Search(prefix);
-            Console.WriteLine("Results: " + string.Join(", ", results));
-        }
-    }
+    public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
+    public List<string> Words = new List<string>();
 }
